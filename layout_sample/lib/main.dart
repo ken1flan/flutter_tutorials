22ead8a5706408e3304a2e7717d88fd213e905f8
ken1flan <ken1flan@gmail.com>
2021-04-02 11:44:21 +0900

    step 1 タイトルバーの追加

--- layout_sample/lib/main.dart
+++ layout_sample/lib/main.dart
@@ -8,14 +8,45 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+        padding: const EdgeInsets.all(32),
+        child: Row(
+          children: [
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(color: Colors.grey[500]),
+                )
+              ],
+            )),
+            Icon(
+              Icons.star,
+              color: Colors.red[500],
+            ),
+            Text('41')
+          ],
+        ));
+
     return MaterialApp(
         title: 'Flutter Layout Demo',
         home: Scaffold(
             appBar: AppBar(
               title: Text('Flutter Layout Demo'),
             ),
-            body: Center(
-              child: Text('Hello, world'),
+            body: Column(
+              children: [
+                titleSection,
+              ],
             )));
   }
 }
