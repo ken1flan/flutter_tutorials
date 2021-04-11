25a7473f47cdbc0bb551a113297223c0e6d9c670
ken1flan <ken1flan@gmail.com>
2021-04-11 17:55:11 +0900

    ファイルを分割

--- layout_sample/test/widget_test.dart
+++ layout_sample/test/widget_test.dart
@@ -8,8 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:layout_sample/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
